e150d5bed38729c3371c7a649597ef60eae376c4
unknown <amitrabani@gmail.com>
2019-09-16 21:16:43 +0300

    add button element

---
+++ src/elements/Button.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export default styled.button`
+    color:#0081f2;
+    width: 30px;
+    height: 100px;
+    background-color: ${(props) => props.theme.primary};
+    `
