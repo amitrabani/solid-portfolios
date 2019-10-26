5b47ac75d7bd281dc42689f06bb993ef28bdca9c
unknown <amitrabani@gmail.com>
2019-10-26 14:07:03 +0200

    add MainPage component

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import MarketSummary from './MarketSummarry';
+// import UserPortfolio from './components/UserPortfolios';
+import UserPortfoliosContainer from '../containers/UserPortfoliosContainer';
+import ExchangeRates from './ExchangeRates';
+import PortfolioNewsContainer from '../containers/PortfolioNewsContainer';
+import StockChart from '../components/StockChart';
+import styled, { ThemeProvider } from 'styled-components'
+
+const Container = styled.div`
+    display: 'flex';
+    flex-flow: 'row wrap';
+    background: ${(props) => props.theme.primaryColor};
+    /* #0000008a */
+
+    *{
+
+
+    }
+`
+function MainPage() {
+    return (
+        <Container>
+            <MarketSummary />
+            <UserPortfoliosContainer />
+            <span style={{display: 'flex'}}>
+            <PortfolioNewsContainer />
+            <div style={{marginLeft: '20px'}}>
+            <ExchangeRates />
+            <StockChart />
+            </div>
+            </span>
+        </Container>
+    );
+}
+
+export default MainPage;
