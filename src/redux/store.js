7ba1652cfbae1c72abf10e47ef1ed80c99ca1d55
unknown <amitrabani@gmail.com>
2019-12-31 23:23:02 +0200

    add store

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers/rootReducer'
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const initialState = { 
+    portfolios: [] 
+  };
+const store =
+    createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(thunk)))
+export default store
