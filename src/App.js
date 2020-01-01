8af0536be358162bea79c678c9ec9b63bfdcc6ce
unknown <amitrabani@gmail.com>
2020-01-01 14:33:55 +0200

    render MainPageContainer instead of MainPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import MainPage from './components/MainPage'
+import MainPageContainer from './components/MainPage/MainPageContainer.js'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 import './App.css';
 
 function App() {
   return (
+      <Provider store={store}>
     <div className="App">
-      <MainPage />
+      <MainPageContainer />
     </div>
+    </Provider>
   );
 }
 
