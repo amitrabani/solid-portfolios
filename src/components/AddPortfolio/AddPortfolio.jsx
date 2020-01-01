aff0e0b94419e7b6df666d5bc0f6fecbb10e9b02
unknown <amitrabani@gmail.com>
2020-01-01 14:49:35 +0200

    add AddPortfolios

---
+++ src/components/AddPortfolio/AddPortfolio.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+import { Container, StyledLogo, IconContainer, Button } from './addPortfolio.js';
+
+const AddPortfolioButton = () => 
+		<Container>
+			<IconContainer>
+				<StyledLogo width="10" height="10" />
+			</IconContainer>
+			<Button>Add portfolio</Button>
+		</Container>
+
+export default AddPortfolioButton;
