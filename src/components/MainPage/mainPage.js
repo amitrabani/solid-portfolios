c68dbdc396e854f43bf30eb670c11536569b86a4
unknown <amitrabani@gmail.com>
2020-01-01 14:29:51 +0200

    add mainPage styles

---
+++ src/components/MainPage/mainPage.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    margin-top:30px;
+    display: inline-flex;;
+`
+export const WideColumn = styled.span`
+    margin: 30px;
+    width: 60%;
+`
+export const NarrowColumn = styled.span`
+    margin: 30px;
+    width: 25%;
+`
