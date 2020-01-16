544d697e5e12a9f33f9fee90d47143890462887d
unknown <amitrabani@gmail.com>
2020-01-16 23:53:33 +0200

    remove double rendering of <tbody/>

--- src/components/ExchangeRates/ExchangeRates.jsx
+++ src/components/ExchangeRates/ExchangeRates.jsx
@@ -5,7 +5,7 @@ const rates = {
   CAD: 1.565,
   CHF: 1.1798,
   GBP: 0.87295,
-  SEK: 10.2983,
+  SEK: 1.2983,
   EUR: 1.092,
   USD: 1.2234
 };
@@ -14,23 +14,17 @@ const renderTableData = currency => {
   return (
     <tr key={currency}>
       <Currency>{currency}</Currency>
-      <CurrencyPrice>{rates[currency].toFixed(4)}</CurrencyPrice>
+      <CurrencyPrice>{rates[currency].toFixed(3)}</CurrencyPrice>
     </tr>
   );
 };
 
 const ExchangeRates = () => {
-  let ratesObjectLength = Object.keys(rates).length;
-  let ratesObjectArray = Object.keys(rates);
+  const ratesObjectArray = Object.keys(rates);
   return (
     <div>
       <Header>CURRENCIES</Header>
       <RatesTable>
-        <tbody>
-          {ratesObjectArray
-            .splice(ratesObjectLength / 2)
-            .map(currency => renderTableData(currency))}
-        </tbody>
         <tbody>
           {ratesObjectArray.map(currency => renderTableData(currency))}
         </tbody>
