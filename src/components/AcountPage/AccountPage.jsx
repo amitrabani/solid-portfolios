ed1893534c3cf20437cf9ded7de034d80af2660a
amit <amitrabani@gmail.com>
2020-04-04 20:22:01 +0300

    add accountPage component

---
+++ src/components/AcountPage/AccountPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import PasswordResetForm from "../PasswordResetForm/PasswordResetForm";
+// import PasswordChangeForm from "../PasswordChangeForm/PasswordChangeForm";
+
+const AccountPage = authUser => (
+  <div>
+    <h1>Page Under Construction</h1>
+    {/* <h1>Account: {authUser.email}</h1> */}
+    {/* <PasswordResetForm /> */}
+    {/* <PasswordChangeForm /> */}
+    {/* <LoginManagement authUser={authUser} /> */}
+  </div>
+);
+
+export default AccountPage
