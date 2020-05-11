1528ad60f47ce20c9e479a0f717337403589ef68
amit <amitrabani@gmail.com>
2020-05-11 18:08:59 +0300

    add new portfolioTasks

---
+++ src/components/user/PortfolioManagement/PortfolioTasks.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import deleteForeverIcon from '../../../icons/deleteForeverIcon.svg';
+import deleteForeverIconHover from '../../../icons/deleteForeverIconHover.svg';
+import AddSymbolFormContainer from '../AddSymbolForm/AddSymbolFormContainer';
+import {
+  CustomDeleteButton, TaskButtonContainer,
+} from './portfolioManagementStyles';
+
+
+const PortfolioTasks = (props) => {
+  const { handleDelete, selectedSymbols, portfolio } = props;
+  return (
+    <TaskButtonContainer>
+      {Object.values(selectedSymbols).some(
+        (value) => value === true,
+      ) ? (
+        <CustomDeleteButton onClick={handleDelete}>
+          <img alt="delete" className="regularIcon" src={deleteForeverIcon} />
+          <img alt="delete" className="hoverIcon" src={deleteForeverIconHover} />
+          Delete Symbols
+        </CustomDeleteButton>
+        ) : (
+          <AddSymbolFormContainer portfolioName={portfolio} />
+        )}
+    </TaskButtonContainer>
+  );
+};
+
+PortfolioTasks.propTypes = {
+  handleDelete: PropTypes.func.isRequired,
+  portfolio: PropTypes.string.isRequired,
+  selectedSymbols: PropTypes.objectOf(PropTypes.bool),
+};
+
+export default PortfolioTasks;
