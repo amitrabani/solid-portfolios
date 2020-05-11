62864f2a7ebc57ba0505daed1c0fd2151dbf7413
amit <amitrabani@gmail.com>
2020-05-11 18:07:31 +0300

    empty acountPage

--- src/components/AcountPage/AccountPage.jsx
+++ src/components/AcountPage/AccountPage.jsx
@@ -1,15 +1,14 @@
-import React from "react";
+import React from 'react';
 // import PasswordResetForm from "../PasswordResetForm/PasswordResetForm";
 // import PasswordChangeForm from "../PasswordChangeForm/PasswordChangeForm";
 
-const AccountPage = authUser => (
+const AccountPage = () => (
   <div>
     <h1>Page Under Construction</h1>
-    {/* <h1>Account: {authUser.email}</h1> */}
+    {/* <h1>Account: {props.user.email}</h1> */}
     {/* <PasswordResetForm /> */}
     {/* <PasswordChangeForm /> */}
-    {/* <LoginManagement authUser={authUser} /> */}
   </div>
 );
 
-export default AccountPage
+export default AccountPage;
