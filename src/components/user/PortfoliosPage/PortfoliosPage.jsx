faf9d75d10714357b69c6893962fe12d9645944b
amit <amitrabani@gmail.com>
2020-05-11 17:53:31 +0300

    render portfoliosList and handle errors

--- src/components/user/PortfoliosPage/PortfoliosPage.jsx
+++ src/components/user/PortfoliosPage/PortfoliosPage.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
-const PortfoliosPage = ()=>{
-    return (
-        <h1>my portfolios:</h1>
-    )
-}
-export default PortfoliosPage
+import PropTypes, { object } from 'prop-types';
+
+import Snackbar from '../../common/snackbar/Snackbar';
+import AddPortfolio from '../AddPortfolio/CreatePortfolioButton';
+import PortfoliosListContainer from '../PortfoliosList/PortfoliosListContainer';
+import PortfoliosHeader from './portfoliosPageStyles';
+
+
+const PortfoliosPage = (props) => {
+  const {
+    portfolios = {},
+    fetchError,
+    isFetchingPortfolios,
+    fetchPortfolios,
+  } = props;
+
+  useEffect(() => {
+    if (portfolios.length === 0) {
+      fetchPortfolios();
+    }
+  }, []);
+
+  return (
+    <div>
+      {fetchError && <Snackbar message="Failed Getting Portfolios" />}
+      <hr />
+      <AddPortfolio />
+      <PortfoliosHeader>My Portfolios:</PortfoliosHeader>
+      {isFetchingPortfolios ? (
+        <h1>Fetching</h1>
+      ) : (
+        <PortfoliosListContainer portfolios={portfolios} />
+      )}
+    </div>
+  );
+};
+
+PortfoliosPage.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  fetchError: PropTypes.any.isRequired,
+  fetchPortfolios: PropTypes.func.isRequired,
+  isFetchingPortfolios: PropTypes.bool,
+  portfolios: PropTypes.objectOf(object),
+};
+
+
+export default PortfoliosPage;
