015374c5a2e52723e3ed68a30db0333d1e1be099
amit <amitrabani@gmail.com>
2020-05-11 17:42:10 +0300

    add user layout hook

---
+++ src/hooks/useLayoutSize.js
@@ -0,0 +1,15 @@
+import { useLayoutEffect, useState } from 'react';
+
+const useLayoutSize = () => {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    const updateSize = () => {
+      setSize([window.innerWidth, window.innerHeight]);
+    };
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+};
+export default useLayoutSize;
