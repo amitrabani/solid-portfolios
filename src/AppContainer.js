7dcddaad2f5ce4fecf50e7d2af80a72322cfde97
amit <amitrabani@gmail.com>
2020-05-11 18:04:46 +0300

    add new appContainer

---
+++ src/AppContainer.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+
+import App from './App';
+import { setUserSignedInSuccess } from './redux/actions/auth/authActions';
+import { fetchPortfolios } from './redux/actions/portfolios/portfoliosActionCreators';
+import { getAuth } from './redux/selectors';
+
+
+const mapDispatchToProps = (dispatch) => ({
+  signIn: (uid) => dispatch(setUserSignedInSuccess(uid)),
+  fetchPortfoliosFromFirestore: () => dispatch(fetchPortfolios()),
+});
+
+const mapStateToProps = (state) => ({
+  auth: getAuth(state),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
