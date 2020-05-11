70510dd2ce42ea411dc713826ded87c8ada5beba
amit <amitrabani@gmail.com>
2020-05-11 17:45:48 +0300

    remove initial state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,13 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers/rootReducer";
-import { reactReduxFirebase } from "react-redux-firebase";
-import firebaseApp from "../components/Firebase/firebase";
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
 
+import rootReducer from './reducers/rootReducer';
 
-// const createStoreWithFirebase = compose(reactReduxFirebase(firebase))(
-//   createStore
-// );
-// const store = createStoreWithFirebase(
-//   reducers,
-//   {},
-//   applyMiddleware(reduxThunk)
-// );
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const initialState = {
-  portfolios: [],
-  isPortfoliosFetching: true
-};
+
 const store = createStore(
   rootReducer,
-  initialState,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk)),
 );
 export default store;
