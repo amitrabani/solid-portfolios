b6249b7d52050de03fb58426cde080cd93fc00f0
amit <amitrabani@gmail.com>
2020-05-11 18:11:06 +0300

    functional signInForm

--- src/components/user/SignInForm/SignInForm.jsx
+++ src/components/user/SignInForm/SignInForm.jsx
@@ -1,72 +1,111 @@
-import React, { Component } from "react";
-// import SignInGoogleButtonContainer from "../SignInGoogle/SignInGoogleButtonContainer";
-// import { signIn } from "../../redux/actions/auth/authActions";
-// import * as ROUTES from "../../constants/routes";
+import React, { useState } from 'react';
 
-const INITIAL_STATE = {
-  email: "",
-  password: "",
-  error: null
-};
-class SignInForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...INITIAL_STATE };
-  }
-  onSubmit = event => {
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+
+import { LANDING } from '../../../constants/routes';
+import {
+  BaseContainer, ButtonsContainer, Form, FormGroup, Header,
+} from '../../../elements/FormStyles';
+import CircularProgress from '../../common/CircularProgress/CircularProgress';
+
+
+// const INITIAL_STATE = {
+//   email: "",
+//   password: "",
+// };
+const SignInForm = (props) => {
+  const { signIn, signUpWithGoogle, auth } = props;
+  const [redirectHome, setRedirectHome] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const { email, password } = this.state;
-    this.props.signIn(email, password);
-    //conditional auth error
-  };
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+    signIn(email, password);
+    setRedirectHome(true);
   };
 
-  render() {
-    const { email, password, error } = this.state;
-    const { authError } = this.props;
+  const handleGoogleLogin = (event) => {
+    event.preventDefault();
+    signUpWithGoogle();
+    setRedirectHome(true);
+  };
 
-    const isInvalid = password === "" || email === "";
-    return (
-      <>
-        {/* const ERROR_CODE_ACCOUNT_EXISTS =
-  "auth/account-exists-with-different-credential";
-const ERROR_MSG_ACCOUNT_EXISTS = `
-  An account with an E-Mail address to
-  this social account already exists. Try to login from
-  this account instead and associate your social accounts on
-  your personal account page.
-`; */}
-        {/* const { signUpWithGoogle } = props; */}
-        {/* // const {error} = this.state; */}
-        <form onSubmit={this.onSubmit}>
-          {/* <button onClick={onSubmitGoogle} disabled={isInvalid} type="submit">
-          SignInGoogle
-        </button> */}
-          <input
-            name="email"
-            value={email}
-            onChange={this.onChange}
-            type="text"
-            placeholder="Email Address"
-          />
-          <input
-            name="password"
-            value={password}
-            onChange={this.onChange}
-            type="password"
-            placeholder="Password"
-          />
-          <button disabled={isInvalid} type="submit">
-            Sign In
-          </button>
-          {error && <p>{error.message}</p>}
-          {/* fix auth error not displayed */}
-          {authError ? <h1>login failed</h1> : <h1>login success</h1>}
-        </form>
-      </>
-    );
+  const isInvalid = password === '' || email === '';
+  let error = auth.authError;
+
+  if (error === "Cannot read property 'uid' of null") {
+    error = 'Network Connection Error';
   }
-}
+
+  return (
+    <>
+      {redirectHome && <Redirect to={LANDING} />}
+
+      {auth.isAuthenticating ? (
+        <CircularProgress />
+      ) : (
+        <BaseContainer style={{ height: '30em' }}>
+          <Form onSubmit={handleSubmit}>
+            <Header>Sign In</Header>
+            <ButtonsContainer>
+              <button type="button" onClick={handleGoogleLogin}>
+                Sign In with Google
+              </button>
+            </ButtonsContainer>
+            <FormGroup>
+              <label htmlFor="Email">Email</label>
+              <input
+                name="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="Email Address"
+              />
+            </FormGroup>
+            <FormGroup>
+              <label htmlFor="Password">Password</label>
+              <input
+                name="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                placeholder="Password"
+              />
+            </FormGroup>
+            <ButtonsContainer>
+              <button
+                style={{ fontSize: '1.2rem' }}
+                disabled={isInvalid}
+                onClick={handleSubmit}
+                type="submit"
+              >
+                Sign In
+              </button>
+            </ButtonsContainer>
+          </Form>
+          <div>
+            {error && (
+              <>
+                <h3>Login Failed</h3>
+                <h5 style={{ color: 'red' }}>{error}</h5>
+              </>
+            )}
+          </div>
+        </BaseContainer>
+      )}
+    </>
+  );
+};
+
+SignInForm.propTypes = {
+  auth: PropTypes.shape({
+    authError: PropTypes.string,
+    isAuthenticating: PropTypes.bool,
+  }),
+  signIn: PropTypes.func.isRequired,
+  signUpWithGoogle: PropTypes.func.isRequired,
+};
+
 export default SignInForm;
