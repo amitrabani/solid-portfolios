9252cd83c95b12ad8185972847f300facbc5df6c
amit <amitrabani@gmail.com>
2020-05-11 17:57:50 +0300

    add 'portfolioListStyles'

---
+++ src/components/user/PortfoliosList/portfoliosListStyles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import Theme from '../../../elements/Theme';
+
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const Header = styled.h2`
+  color: #ffffffd4;
+  border-bottom: 5px solid ${Theme.primaryColor};
+`;
