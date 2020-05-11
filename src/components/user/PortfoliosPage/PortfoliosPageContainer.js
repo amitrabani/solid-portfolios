da2384a622b1d3b1137e8f98e881a8955acea590
amit <amitrabani@gmail.com>
2020-05-11 17:52:37 +0300

    add portfolioPageContainer

--- src/components/user/PortfoliosPage/PortfoliosPageContainer.js
+++ src/components/user/PortfoliosPage/PortfoliosPageContainer.js
@@ -1,10 +1,23 @@
-import { connect } from "react-redux";
-import PortfoliosPage from "./PortfoliosPage";
+import { connect } from 'react-redux';
 
-const mapStateToProps = state => {
+import {
+  fetchPortfolios,
+} from '../../../redux/actions/portfolios/portfoliosActionCreators';
+import { getPortfolios } from '../../../redux/selectors';
+import PortfoliosPage from './PortfoliosPage';
+
+const mapDispatchToProps = { fetchPortfolios };
+
+const mapStateToProps = (state) => {
+  const {
+    fetchingPortfoliosError,
+    isFetchingPortfolios,
+    portfolios,
+  } = getPortfolios(state);
   return {
-
+    fetchError: fetchingPortfoliosError,
+    isFetchingPortfolios,
+    portfolios,
   };
 };
-
-export default connect(null)(PortfoliosPage);
+export default connect(mapStateToProps, mapDispatchToProps)(PortfoliosPage);
