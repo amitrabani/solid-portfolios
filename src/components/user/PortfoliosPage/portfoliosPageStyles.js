4abd3a1f7834c4495ef114b1a888c798dafd5d5b
amit <amitrabani@gmail.com>
2020-05-11 17:52:19 +0300

    add portfolioPageStyles

---
+++ src/components/user/PortfoliosPage/portfoliosPageStyles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import { Header } from '../../../elements/FormStyles';
+
+export default styled(Header)`
+  color: white;
+  margin: 1em;
+  text-align: left;
+  position: relative;
+`;
