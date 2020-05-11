9774624fc6d049ffec1055bc6f01ea0c00c38bec
amit <amitrabani@gmail.com>
2020-05-11 18:05:28 +0300

    add new stockApi

---
+++ src/api/StocksApi.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+export const fetchQuote = (symbol) => axios
+  .get(
+    `https://finnhub-realtime-stock-price.p.rapidapi.com/quote?symbol=${symbol}`,
+    {
+      headers: {
+        'x-rapidapi-host': 'finnhub-realtime-stock-price.p.rapidapi.com',
+        'x-rapidapi-key':
+          '538f35aacbmsh7a007be0718027bp13effdjsn802e4e45cfeb',
+      },
+    },
+  )
+  .then((response) => {
+    if (response.data === 'Symbol not supported') {
+      throw new Error(response.data);
+    } else {
+      // extract to toQuoteModel
+      const quote = {
+        current: response.data.c,
+        todaysHigh: response.data.h,
+        todaysLow: response.data.l,
+        previeousClose: response.data.pc,
+        symbol,
+      };
+      return quote;
+    }
+  })
+  .catch((error) => error);
+
+export const fetchMarketNews = () => axios
+  .get('https://finnhub.io/api/v1/news/?token=bqhk217rh5rdcs9r2h30')
+  .then((body) => body.data)
+  .catch((error) => error);
