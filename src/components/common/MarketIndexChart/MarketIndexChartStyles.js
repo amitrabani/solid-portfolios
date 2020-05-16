4d0225dd17fcc5df459f1fb6682dc8e582463e32
amit <amitrabani@gmail.com>
2020-05-16 15:26:25 +0300

    add marketIndexChartStyles

---
+++ src/components/common/MarketIndexChart/MarketIndexChartStyles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  margin-top: 30px;
+  position: relative;
+  width: 100%;
+  height: fit-content;
+  border-width: 2px;
+  border-style: solid;
+  border-color: #80808059;
+  border-radius: 10px;
+`;
