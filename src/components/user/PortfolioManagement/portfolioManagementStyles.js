cdb895619585d2867ad777b316ce2b271d4ecbe2
amit <amitrabani@gmail.com>
2020-05-16 15:41:41 +0300

    add symbolTD styles

--- src/components/user/PortfolioManagement/portfolioManagementStyles.js
+++ src/components/user/PortfolioManagement/portfolioManagementStyles.js
@@ -5,6 +5,10 @@ import DeleteButton from '../../../elements/DeleteButton';
 import { Header } from '../../../elements/FormStyles';
 import Theme from '../../../elements/Theme';
 
+export const SymbolTd = styled.td`
+font-weight:600;
+color: ${Theme.primaryColor};
+`;
 
 export const PortfolioHeader = styled(Header)`
   color: white;
@@ -14,15 +18,16 @@ export const PortfolioHeader = styled(Header)`
 `;
 
 export const TaskButtonContainer = styled.div`
-height:44px;
 display: flex;
 justify-content: flex-start;
 margin: 1em;
 `;
+
 export const CustomDeleteButton = styled(DeleteButton)`
   margin:1em;
   display: flex;
   align-items: center;
+
   :hover{
     color:${Theme.negativeColor}
   }
