5c96d6680db63d19b75e655c4c1956d1ae28e471
amit <amitrabani@gmail.com>
2020-05-16 15:18:22 +0300

    rename mainPage.js to mainPageStyles

--- src/components/common/MainPage/mainPageStyles.js
+++ src/components/common/MainPage/mainPageStyles.js
@@ -9,15 +9,11 @@ export const ColumnsContainer = styled.div`
   }
 `;
 
-// }
-
 export const WideColumn = styled.section`
   width: 65%;
   margin: 1em;
-  /* display: flex;
-  flex-direction: column;
-  justify-content: space-around; */
 `;
+
 export const NarrowColumn = styled.section`
   margin: 1em;
   display: flex;
