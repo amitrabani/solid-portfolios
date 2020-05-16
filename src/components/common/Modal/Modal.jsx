9cccdf252a63b3dd872c7a19fb5178ab28c11a88
amit <amitrabani@gmail.com>
2020-05-16 15:33:50 +0300

    remove comments and sort imports

--- src/components/common/Modal/Modal.jsx
+++ src/components/common/Modal/Modal.jsx
@@ -1,97 +1,33 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Container } from "../Modal/modalStyles";
-import SignInFormContainer from "../../user/SignInForm/SignInFormContainer";
-import { signUpWithGoogle } from "../../../redux/actions/auth/authActionCreators";
-import { compose } from "recompose";
-import { connect } from "react-redux";
-import SignUpFormContainer from "../../user/SignUp/SignUpFormContainer";
+import React, { useEffect } from 'react';
 
-const Modal = props => {
-  const RENDER_SIGNIN = "signIn";
-  const RENDER_SIGNUP = "SignUp";
-  const RENDER_FORGOT = "forgotPassword";
+import PropTypes from 'prop-types';
 
-  const [isComponentVisible, setIsComponentVisible] = useState(props.isOpen);
-  const [renderedForm, setrenderedForm] = useState(RENDER_SIGNIN);
+import useComponentVisible from '../../../hooks/useComponentVisable';
+import { ModalContainer, ModalContent } from './modalStyles';
 
-  const ref = useRef(null);
-  const handleHideDropdown = (event: KeyboardEvent) => {
-    if (event.key === "Escape") {
-      props.onClose();
-      setIsComponentVisible(false);
-    }
-  };
 
-  const handleClickOutside = event => {
-    if (ref.current && !ref.current.contains(event.target)) {
-      props.onClose();
-      setIsComponentVisible(false);
-    }
-  };
+const Modal = (props) => {
+  const { onClose, children } = props;
+  const { ref, isComponentVisible } = useComponentVisible(true);
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    props.signInGoogle();
-  };
-
-  const switchForm = form => {
-    setrenderedForm(form);
-  };
-  useEffect(() => {
-    setIsComponentVisible(props.isOpen);
-    document.addEventListener("keydown", handleHideDropdown, true);
-    document.addEventListener("click", handleClickOutside, true);
-    return () => {
-      document.removeEventListener("keydown", handleHideDropdown, true);
-      document.removeEventListener("click", handleClickOutside, true);
-    };
-  }, [props.isOpen]);
+  useEffect(() => () => {
+    onClose();
+  }, [isComponentVisible]);
 
   return (
-    <>
-      {isComponentVisible && (
-        <Container ref={ref}>
-          {renderedForm === RENDER_SIGNIN && (
-            <>
-              <button onClick={handleSubmit}>Sign In with Google</button>
-              <SignInFormContainer firebase={props.authUser} />
-              <a onClick={() => switchForm(RENDER_SIGNUP)}>Sign Up Instead</a>
-            </>
-          )}
-          {renderedForm === RENDER_SIGNUP && (
-            <>
-              <SignUpFormContainer />
-              <a onClick={() => switchForm(RENDER_SIGNIN)}>Sign In Instead</a>
-            </>
-          )}
-
-          {/* {renderedForm === RENDER_FORGOT && (
-            <>
-              <SignUpFormContainer />
-              <a onClick={() => switchForm(RENDER_SIGNIN)}>Sign In Instead</a>
-            </>
-          )} */}
-        </Container>
-      )}
-    </>
+    isComponentVisible && (
+      <ModalContainer id="modalContainer">
+        {' '}
+        <ModalContent id="content" ref={ref}>
+          {children}
+        </ModalContent>
+      </ModalContainer>
+    )
   );
 };
 
-const mapStateToProps = state => ({
-  // authUser: state.sessionState.authUser
-});
-
-const mapDispatchToProps = dispatch => {
-  return {
-    signInGoogle: () => dispatch(signUpWithGoogle())
-  };
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
 };
 
-// const condition = authUser => !!authUser;
-export default compose(
-  connect(mapStateToProps, mapDispatchToProps)
-  // withEmailVerification,
-  // withAuthorization(condition),
-)(Modal);
-
-// export default Modal;
+export default Modal;
