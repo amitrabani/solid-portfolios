ed21f98f09a1cb3e864cfd3fa312206c81a21c6f
amit <amitrabani@gmail.com>
2020-05-16 15:33:13 +0300

    shorter mapDispatchToProps

--- src/components/common/MarketNews/MarketNewsContainer.jsx
+++ src/components/common/MarketNews/MarketNewsContainer.jsx
@@ -4,11 +4,8 @@ import getMarketNews from '../../../redux/actions/market/marketActionCreators';
 import { getNews } from '../../../redux/selectors';
 import MarketNews from './MarketNews';
 
-const mapDispatchToProps = (dispatch) => ({
-
-
-  fetchNews: () => dispatch(getMarketNews()),
-});
+const mapDispatchToProps = { getMarketNews };
 
 const mapStateToProps = (state) => ({ news: getNews(state) });
+
 export default connect(mapStateToProps, mapDispatchToProps)(MarketNews);
