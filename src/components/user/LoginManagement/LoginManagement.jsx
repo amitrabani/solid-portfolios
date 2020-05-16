6bbd2562521359160686ae9e909f73ae28ab5b70
amit <amitrabani@gmail.com>
2020-05-16 15:40:07 +0300

    sort imports

--- src/components/user/LoginManagement/LoginManagement.jsx
+++ src/components/user/LoginManagement/LoginManagement.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { Redirect } from 'react-router-dom';
 
 import SignInFormContainer from '../SignInForm/SignInFormContainer';
 import SignUpFormContainer from '../SignUp/SignUpFormContainer';
 import { FormChanger, Login } from './loginManagementStyles';
 
+
 const LoginManagement = () => {
   const RENDER_SIGNIN = 'signIn';
   const RENDER_SIGNUP = 'signUp';
+  const [renderedForm, setRenderedForm] = useState(RENDER_SIGNIN);
+  const [shouldRedirect, setShouldRedirect] = useState(false);
+
+  useEffect(() => () => {
+    setShouldRedirect(true);
+  }, []);
+
+
   // const RENDER_FORGOT = "forgotPassword";
 
-  const [renderedForm, setRenderedForm] = useState(RENDER_SIGNIN);
 
   const switchForm = () => (renderedForm === RENDER_SIGNIN
     ? setRenderedForm(RENDER_SIGNUP)
     : setRenderedForm(RENDER_SIGNIN));
 
+  if (shouldRedirect) {
+    return <Redirect to="/" />;
+  }
   return (
     <div style={{ width: '50em', display: 'flex', justifyContent: 'center' }}>
       <Login>
