b73b971d5f20d39c66aaae0460dfe2501e4055e4
amit <amitrabani@gmail.com>
2020-05-16 15:22:57 +0300

    add chart options

--- src/components/common/MarketIndexChart/MarketIndexChart.jsx
+++ src/components/common/MarketIndexChart/MarketIndexChart.jsx
@@ -1,29 +1,52 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
-var CanvasJSReact = require("../../../canvasjs.react").default;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+import React from 'react';
 
-const Container = styled.div`
-  margin-top: 30px;
-  position: relative;
-  width: 100%;
-  height: fit-content;
-  border-width: 2px;
-  border-style: solid;
-  border-color: #80808059;
-  border-radius: 10px;
-`;
+import Theme from '../../../elements/Theme';
+import Container from './MarketIndexChartStyles';
 
-const MarketIndexChart = props => {
-  return (
-    <Container>
-      <CanvasJSChart width="500px" height="500px" options={props.options} />
-    </Container>
-  );
+
+const CanvasJSReact = require('../../../canvasjs.react').default;
+
+const { CanvasJSChart } = CanvasJSReact;
+
+const options = {
+  animationEnabled: true,
+  animationDuration: 2000,
+  backgroundColor: 'transparent',
+  title: {
+    text: 'S&P 500',
+    fontSize: 25,
+    fontColor: 'white',
+  },
+  axisX: {
+    lineThickness: 1,
+  },
+  axisY: {
+    lineThickness: 1,
+    gridColor: 'transparent',
+  },
+  data: [
+    {
+      type: 'splineArea',
+      interactivityEnabled: false,
+      lineThickness: 4,
+      color: 1 > 0 ? Theme.positiveColor : Theme.negativeColor,
+      markerSize: 3,
+      markerType: 'circle',
+      markerColor: '#0080005e',
+      fillOpacity: 1,
+      dataPoints: [
+        { x: 1, y: 77 },
+        { x: 2, y: 8 },
+        { x: 3, y: 9 },
+        { x: 4, y: 4 },
+      ],
+    },
+  ],
 };
+
+const MarketIndexChart = () => (
+  <Container>
+    <CanvasJSChart width="500px" height="500px" options={options} />
+  </Container>
+);
 export default MarketIndexChart;
-
-MarketIndexChart.propTypes = {
-  options: PropTypes.object.isRequired
-};
