8a53581ded266a52be9abee51287e2517605ad37
amit <amitrabani@gmail.com>
2020-05-19 15:55:30 +0300

    remove comment

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,5 @@
 export const LANDING = '/';
 export const ACCOUNT = '/account';
-// export const ADMIN = '/admin';
 export const PORTFOLIO_MANAGEMENT = '/portfolios/:id';
 export const PORTFOLIOS = '/portfolios';
 export const BLOG = '/blog';
