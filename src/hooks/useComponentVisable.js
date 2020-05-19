7cae18488cf3971750ebcae979a91d851313b5b9
amit <amitrabani@gmail.com>
2020-05-19 18:37:51 +0300

    remove Escape button event listener

--- src/hooks/useComponentVisable.js
+++ src/hooks/useComponentVisable.js
@@ -6,12 +6,6 @@ export default (initialIsVisible) => {
   );
   const ref = useRef(null);
 
-  const handleHideComponent = (event) => {
-    if (event.key === 'Escape') {
-      setIsComponentVisible(false);
-    }
-  };
-
   const handleClickOutside = (event) => {
     if (ref.current && !ref.current.contains(event.target)) {
       setIsComponentVisible(false);
@@ -19,10 +13,8 @@ export default (initialIsVisible) => {
   };
 
   useEffect(() => {
-    document.addEventListener('keydown', handleHideComponent, true);
     document.addEventListener('click', handleClickOutside, true);
     return () => {
-      document.removeEventListener('keydown', handleHideComponent, true);
       document.removeEventListener('click', handleClickOutside, true);
     };
   });
